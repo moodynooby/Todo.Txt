4551d33f9ce715efb00f041bf6d2e8d23ba1b159
Manas Doshi <immanasdoshi@gmail.com>
2025-06-19 23:15:37 +0530

    typo error

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Header from './header';
+import Header from './Header';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
